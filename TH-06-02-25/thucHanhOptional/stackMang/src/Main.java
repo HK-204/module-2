695b5f827bd4ca35787633234582d0e10e79dbe7
HK-204 <haidao2004bt@gmail,com>
2025-06-01 23:44:58 +0700

    update

--- TH-06-02-25/thucHanhOptional/stackMang/src/Main.java
+++ TH-06-02-25/thucHanhOptional/stackMang/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         stack.push(1);
 
         System.out.println("1. Size of stack after push operations: " + stack.size());
-        System.out.printf("2. Pop elements from stack : ");
+        System.out.print("2. Pop elements from stack : ");
 
         while (!stack.isEmpty()) {
             System.out.printf(" %d", stack.pop());
